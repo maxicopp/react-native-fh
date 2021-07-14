dd1c7eff1a198ef92350777e39f1bad6a4b30043
maxicopp <maxicopp@outlook.com>
2021-07-14 00:53:37 -0300

    fadeIn

---
+++ 05-PeliculasApp/src/screens/FadeScreen.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, { useRef } from 'react';
+import { View, Animated, Button } from 'react-native';
+
+export const FadeScreen = () => {
+  const opacity = useRef(new Animated.Value(0)).current;
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'grey',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Animated.View
+        style={{
+          backgroundColor: '#084F6A',
+          width: 150,
+          height: 150,
+          borderColor: 'white',
+          borderWidth: 10,
+          marginBottom: 10,
+          opacity: opacity,
+        }}
+      />
+      <Button title="fadeIn" onPress={fadeIn} />
+    </View>
+  );
+};
