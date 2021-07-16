dc15ae4a46d5d69e072d9fe2fc941ef6633b5d9a
Maximiliano Cóppola <maxicopp@outlook.com>
2021-07-16 00:54:04 -0300

    Obtener los colores de las imágenes

--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ActivityIndicator, Dimensions, View, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import ImageColors from 'react-native-image-colors';
 
 import { useMovies } from '../hooks/useMovies';
 import { MoviePoster } from '../components/MoviePoster';
@@ -14,6 +15,12 @@ const { width: windowWidth } = Dimensions.get('window');
 export const HomeScreen = () => {
   const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
   const { top } = useSafeAreaInsets();
+  const getPosterColors = async (index: number) => {
+    const movie = nowPlaying[index];
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const colors = await ImageColors.getColors(uri, {});
+    console.log(colors);
+  };
   if (isLoading) {
     return (
       <View
@@ -34,6 +41,7 @@ export const HomeScreen = () => {
               sliderWidth={windowWidth}
               itemWidth={300}
               inactiveSlideOpacity={0.9}
+              onSnapToItem={index => getPosterColors(index)}
             />
           </View>
           {/* Películas Populares */}
