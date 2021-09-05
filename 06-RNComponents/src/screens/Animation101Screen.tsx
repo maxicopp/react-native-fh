314d57d85fb6067fe98bb6eb3059f748446f0317
Maximiliano Cóppola <maxicopp@outlook.com>
2021-09-05 05:18:44 -0300

    fadeIn y fadeOut

--- 06-RNComponents/src/screens/Animation101Screen.tsx
+++ 06-RNComponents/src/screens/Animation101Screen.tsx
@@ -1,11 +1,30 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useRef } from 'react';
-import { Animated, StyleSheet, View } from 'react-native';
+import { Animated, Button, StyleSheet, View } from 'react-native';
 
 export const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0.4)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => console.log('animación terminó'));
+  };
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
   return (
     <View style={styles.container}>
-      <Animated.View style={{ ...styles.purpleBox, opacity }} />
+      <Animated.View
+        style={{ ...styles.purpleBox, opacity, marginBottom: 20 }}
+      />
+      <Button title="fadeIn" onPress={fadeIn} />
+      <Button title="fadeOut" onPress={fadeOut} />
     </View>
   );
 };
