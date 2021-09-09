3c21f0a1379e9e9db64e1ddded7dd0d845d6389d
Maximiliano Cóppola <maxicopp@outlook.com>
2021-09-09 02:37:12 -0300

    CustomSwitch

--- 06-RNComponents/src/screens/SwitchScreen.tsx
+++ 06-RNComponents/src/screens/SwitchScreen.tsx
@@ -1,20 +1,44 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { Platform, Switch, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { CustomSwitch } from '../components/CustomSwitch';
 import { HeaderTitle } from '../components/HeaderTitle';
 
 export const SwitchScreen = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(!isEnabled);
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+  const { isActive } = state;
+  const onChange = (value: boolean, field: string) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
   return (
     <View style={{ marginHorizontal: 20 }}>
       <HeaderTitle title="Switches" />
-      <Switch
-        trackColor={{ false: '#D9D9DB', true: '#5856D6' }}
-        thumbColor={Platform.OS === 'android' ? '#5856d6' : ''}
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isActive</Text>
+        <CustomSwitch
+          isOn={isActive}
+          onChange={value => onChange(value, 'isActive')}
+        />
+      </View>
+      <Text style={styles.switchText}>{JSON.stringify(state, null, 5)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  switchText: {
+    fontSize: 25,
+  },
+});
