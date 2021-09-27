520b888293208372d05b341deb13ff1eb0f7bb10
Maximiliano Cóppola <maxicopp@outlook.com>
2021-09-26 23:58:15 -0300

    Header y Footer de un SectionList

--- 06-RNComponents/src/screens/CustomSectionListScreen.tsx
+++ 06-RNComponents/src/screens/CustomSectionListScreen.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { SectionList, Text, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
+import { ItemSeparator } from '../components/ItemSeparator';
 import { styles } from '../theme/appTheme';
 
 interface Casas {
@@ -80,6 +81,12 @@ export const CustomSectionListScreen = () => {
       <SectionList
         sections={casas}
         keyExtractor={(item, index) => item + index}
+        ListHeaderComponent={() => <HeaderTitle title="Section List" />}
+        ListFooterComponent={() => (
+          <View style={{ marginBottom: 70 }}>
+            <HeaderTitle title={'Total de casas ' + casas.length} />
+          </View>
+        )}
         renderItem={({ item }) => <Text>{item}</Text>}
         stickySectionHeadersEnabled
         renderSectionHeader={({ section }) => (
@@ -87,6 +94,12 @@ export const CustomSectionListScreen = () => {
             <HeaderTitle title={section.casa} />
           </View>
         )}
+        renderSectionFooter={({ section }) => (
+          <HeaderTitle title={'Total: ' + section.data.length} />
+        )}
+        SectionSeparatorComponent={() => <ItemSeparator />}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   );
