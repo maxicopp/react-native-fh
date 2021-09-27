e542d26bbffe531f6e6d017ae77bc2f2345c362d
Maximiliano Cóppola <maxicopp@outlook.com>
2021-09-27 01:15:28 -0300

    Personalizar el modal

--- 06-RNComponents/src/screens/ModalScreen.tsx
+++ 06-RNComponents/src/screens/ModalScreen.tsx
@@ -11,10 +11,32 @@ export const ModalScreen = () => {
       <HeaderTitle title="Modal Screen" />
       <Button title="Abrir modal" onPress={() => setIsVisible(true)} />
       <Modal animationType="fade" visible={isVisible} transparent={true}>
-        <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.3)' }}>
-          <View style={{ backgroundColor: 'white' }}>
-            <HeaderTitle title="Modal" />
-            <Text>Cuerpo del modal</Text>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              backgroundColor: 'white',
+              width: 200,
+              height: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowOffset: {
+                width: 0,
+                height: 10,
+              },
+              shadowOpacity: 0.25,
+              elevation: 10,
+              borderRadius: 5,
+            }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Modal</Text>
+            <Text style={{ fontSize: 16, fontWeight: '300', marginBottom: 20 }}>
+              Cuerpo del modal
+            </Text>
             <Button title="Cerrar" onPress={() => setIsVisible(false)} />
           </View>
         </View>
