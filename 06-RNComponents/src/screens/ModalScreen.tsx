ab4bd54f04db62c72421d52e4832995acac22742
Maximiliano Cóppola <maxicopp@outlook.com>
2021-09-27 01:04:46 -0300

    Componente - Modal

---
+++ 06-RNComponents/src/screens/ModalScreen.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { Button, Modal, Text, View } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
+import { styles } from '../theme/appTheme';
+
+export const ModalScreen = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <View style={styles.globalMargin}>
+      <HeaderTitle title="Modal Screen" />
+      <Button title="Abrir modal" onPress={() => setIsVisible(true)} />
+      <Modal animationType="fade" visible={isVisible} transparent={true}>
+        <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.3)' }}>
+          <View style={{ backgroundColor: 'white' }}>
+            <HeaderTitle title="Modal" />
+            <Text>Cuerpo del modal</Text>
+            <Button title="Cerrar" onPress={() => setIsVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
