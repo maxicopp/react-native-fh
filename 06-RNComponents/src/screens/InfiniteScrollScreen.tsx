236b3dc95122ec61d41c49aab554ad3aa6fa4017
Maximiliano Cóppola <maxicopp@outlook.com>
2021-09-27 02:12:34 -0300

    InfiniteScroll con imágenes

--- 06-RNComponents/src/screens/InfiniteScrollScreen.tsx
+++ 06-RNComponents/src/screens/InfiniteScrollScreen.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Image, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 
 export const InfiniteScrollScreen = () => {
@@ -10,11 +10,16 @@ export const InfiniteScrollScreen = () => {
     for (let i = 0; i < 5; i++) {
       newArray[i] = numbers.length + i;
     }
-    setNumbers([...numbers, ...newArray]);
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1500);
   };
-  const renderItem = (item: number) => {
-    return <Text style={styles.textItem}>{item}</Text>;
-  };
+  const renderItem = (item: number) => (
+    <Image
+      source={{ uri: `https://picsum.photos/id/${item}/500/400` }}
+      style={{ width: '100%', height: 400 }}
+    />
+  );
   return (
     <View style={{ flex: 1, backgroundColor: 'red' }}>
       <FlatList
@@ -24,14 +29,18 @@ export const InfiniteScrollScreen = () => {
         ListHeaderComponent={<HeaderTitle title="Infinite Scroll" />}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
+        ListFooterComponent={() => (
+          <View
+            style={{
+              height: 150,
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <ActivityIndicator size={25} color="#5856D6" />
+          </View>
+        )}
       />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  textItem: {
-    backgroundColor: 'green',
-    height: 150,
-  },
-});
