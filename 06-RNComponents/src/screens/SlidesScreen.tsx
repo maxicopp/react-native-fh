6487c6efcc4d8b4e0dfa931f6c7021f9e0a06e43
Maximiliano Cóppola <maxicopp@outlook.com>
2021-10-07 01:47:29 -0300

    Botón en el último slide

--- 06-RNComponents/src/screens/SlidesScreen.tsx
+++ 06-RNComponents/src/screens/SlidesScreen.tsx
@@ -7,9 +7,11 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const { width: screenWidth } = Dimensions.get('window');
 
@@ -68,16 +70,38 @@ export const SlidesScreen = () => {
         layout="default"
         onSnapToItem={index => setActiveIndex(index)}
       />
-      <Pagination
-        dotsLength={items.length}
-        activeDotIndex={activeIndex}
-        dotStyle={{
-          width: 10,
-          height: 10,
-          borderRadius: 10,
-          backgroundColor: '#5856D6',
-        }}
-      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginHorizontal: 20,
+          alignItems: 'center',
+        }}>
+        <Pagination
+          dotsLength={items.length}
+          activeDotIndex={activeIndex}
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 10,
+            backgroundColor: '#5856D6',
+          }}
+        />
+        <TouchableOpacity
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#5856D6',
+            width: 140,
+            height: 50,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          activeOpacity={0.8}>
+          <Text style={{ fontSize: 25, color: 'white' }}>Entrar</Text>
+          <Icon name="chevron-forward-outline" color="white" size={30} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
