a346f7bbdd27abeb8b55fd205af58d5b15fd632a
Maximiliano Cóppola <maxicopp@outlook.com>
2021-11-08 07:42:08 -0300

    Cambio de tema basado en el sistema operativo

--- 06-RNComponents/src/context/themeContext/ThemeContext.tsx
+++ 06-RNComponents/src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,11 @@
-import React, { createContext, useReducer } from 'react';
-import { lightTheme, themeReducer, ThemeState } from './themeReducer';
+import React, { createContext, useEffect, useReducer } from 'react';
+import { Appearance, AppState } from 'react-native';
+import {
+  darkTheme,
+  lightTheme,
+  themeReducer,
+  ThemeState,
+} from './themeReducer';
 
 interface ThemeContextProps {
   theme: ThemeState;
@@ -10,7 +16,19 @@ interface ThemeContextProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
-  const [theme, dispatch] = useReducer(themeReducer, lightTheme); // TODO: leer el tema global
+  const [theme, dispatch] = useReducer(
+    themeReducer,
+    Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme,
+  );
+  useEffect(() => {
+    AppState.addEventListener('change', status => {
+      if (status === 'active') {
+        Appearance.getColorScheme() === 'light'
+          ? setLightTheme()
+          : setDarkTheme();
+      }
+    });
+  }, []);
   const setDarkTheme = () => {
     dispatch({ type: 'set_dark_theme' });
     console.log('setDarkTheme');
