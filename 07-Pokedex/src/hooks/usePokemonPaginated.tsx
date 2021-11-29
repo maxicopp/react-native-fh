4a685d82bf5c3b0188024baa7572dad96de3d5f5
Maximiliano Cóppola <maxicopp@outlook.com>
2021-11-29 03:05:57 -0300

    Mapear resultado a un arreglo de SimplePokemon

--- 07-Pokedex/src/hooks/usePokemonPaginated.tsx
+++ 07-Pokedex/src/hooks/usePokemonPaginated.tsx
@@ -8,11 +8,13 @@ import {
 } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
     [],
   );
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
   const loadPokemons = async () => {
+    setIsLoading(true);
     const resp = await pokemonApi.get<PokemonPaginatedResponse>(
       nextPageUrl.current,
     );
@@ -20,13 +22,25 @@ export const usePokemonPaginated = () => {
     mapPokemonList(resp.data.results);
   };
   const mapPokemonList = (pokemonList: Result[]) => {
-    pokemonList.forEach(poke => console.log(poke.name));
+    const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+      const urlParts = url.split('/');
+      const id = urlParts[urlParts.length - 2];
+      const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+      return {
+        id,
+        name,
+        picture,
+      };
+    });
+    setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
+    setIsLoading(false);
   };
   useEffect(() => {
     loadPokemons();
   }, []);
 
   return {
+    isLoading,
     simplePokemonList,
   };
 };
