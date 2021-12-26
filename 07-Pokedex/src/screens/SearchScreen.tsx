4b315a2a38ccea82832a3893ab55ea9ee413d268
Maximiliano Cóppola <maxicopp@outlook.com>
2021-12-26 10:12:57 -0300

    Pre-cargar la información para buscar por nombre

--- 07-Pokedex/src/screens/SearchScreen.tsx
+++ 07-Pokedex/src/screens/SearchScreen.tsx
@@ -1,11 +1,30 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { Platform, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../components/PokemonCard';
 import { SearchInput } from '../components/SearchInput';
+import { usePokemonSearch } from '../hooks/usePokemonSearch';
+import { styles as globalStyles } from '../theme/appTheme';
 
 export const SearchScreen = () => {
   const { top } = useSafeAreaInsets();
+  const { isFetching, simplePokemonList } = usePokemonSearch();
+  if (isFetching) {
+    return (
+      <View style={styles.activityContainer}>
+        <ActivityIndicator size={50} color="grey" />
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
   return (
     <View
       style={{
@@ -14,6 +33,32 @@ export const SearchScreen = () => {
         marginHorizontal: 20,
       }}>
       <SearchInput />
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        // Header
+        ListHeaderComponent={
+          <Text
+            style={{
+              ...globalStyles.title,
+              ...globalStyles.globalMargin,
+              paddingBottom: 10,
+            }}>
+            Pokedex
+          </Text>
+        }
+        renderItem={({ item }) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  activityContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
