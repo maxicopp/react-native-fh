0e81714d4514c7558dda130825a54c128f963235
Maximiliano Cóppola <maxicopp@outlook.com>
2022-01-09 15:51:56 -0300

    Permission Provider - ContextAPI

--- 08-RutasApp/App.tsx
+++ 08-RutasApp/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigator/Navigator';
+import { PermissionsProvider } from './src/context/PermissionsContext';
+
+const AppState = ({ children }: any) => {
+  return <PermissionsProvider>{children}</PermissionsProvider>;
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator />
+      <AppState>
+        <Navigator />
+      </AppState>
     </NavigationContainer>
   );
 };
