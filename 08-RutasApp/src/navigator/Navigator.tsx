dc993a2972282dcdd1b9e3b3f41fe37484d7c991
Maximiliano Cóppola <maxicopp@outlook.com>
2022-01-09 18:09:48 -0300

    Detectar qué pantalla mostrar al inicio de forma automática

--- 08-RutasApp/src/navigator/Navigator.tsx
+++ 08-RutasApp/src/navigator/Navigator.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { PermissionsScreen } from '../pages/PermissionsScreen';
 import { MapScreen } from '../pages/MapScreen';
+import { PermissionsContext } from '../context/PermissionsContext';
+import { LoadingScreen } from '../pages/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 export const Navigator = () => {
+  const { permissions } = useContext(PermissionsContext);
+  if (permissions.locationStatus === 'unavailable') {
+    return <LoadingScreen />;
+  }
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
         cardStyle: { backgroundColor: 'white' },
       }}>
-      <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
-      <Stack.Screen name="MapScreen" component={MapScreen} />
+      {permissions.locationStatus === 'granted' ? (
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+      ) : (
+        <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      )}
     </Stack.Navigator>
   );
 };
