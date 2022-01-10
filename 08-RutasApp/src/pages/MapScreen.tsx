476f159146614ff41f1e31fca358523dbc9c8e7c
Maximiliano Cóppola <maxicopp@outlook.com>
2022-01-10 00:39:36 -0300

    Android: Configuración de GoogleMaps

--- 08-RutasApp/src/pages/MapScreen.tsx
+++ 08-RutasApp/src/pages/MapScreen.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
