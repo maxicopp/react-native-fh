7be7ff1f0120b6dfa616ed0a62b4ae2b35dd63da
Maximiliano Cóppola <maxicopp@outlook.com>
2022-01-15 04:42:56 -0300

    Obtener las coordenadas del usuario

--- 08-RutasApp/src/components/Map.tsx
+++ 08-RutasApp/src/components/Map.tsx
@@ -1,8 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useEffect } from 'react';
 import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 export const Map = () => {
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => console.log(info),
+      err => console.log({ err }),
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  }, []);
   return (
     <>
       <MapView
