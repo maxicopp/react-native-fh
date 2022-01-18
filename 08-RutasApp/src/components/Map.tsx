b52d1ceffcedf2a7b05169e7d5655ef5401a7dc0
Maximiliano Cóppola <maxicopp@outlook.com>
2022-01-18 04:25:07 -0300

    Dale seguimiento al usuario (Mover cámara constantemente)

--- 08-RutasApp/src/components/Map.tsx
+++ 08-RutasApp/src/components/Map.tsx
@@ -1,13 +1,30 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import MapView from 'react-native-maps';
 import { useLocation } from '../hooks/useLocation';
 import { LoadingScreen } from '../pages/LoadingScreen';
 import { Fab } from './Fab';
 
 export const Map = () => {
-  const { hasLocation, initialPosition, getCurrentLocation } = useLocation();
+  const {
+    hasLocation,
+    initialPosition,
+    getCurrentLocation,
+    followUserLocation,
+    userLocation,
+  } = useLocation();
   const mapViewRef = useRef<MapView>();
+  useEffect(() => {
+    followUserLocation();
+    return () => {
+      // TODO: Cancelar el seguimiento
+    };
+  }, []);
+  useEffect(() => {
+    mapViewRef.current?.animateCamera({
+      center: userLocation,
+    });
+  }, [userLocation]);
   const centerPosition = async () => {
     const location = await getCurrentLocation();
     mapViewRef.current?.animateCamera({
--- 08-RutasApp/src/hooks/useLocation.tsx
+++ 08-RutasApp/src/hooks/useLocation.tsx
@@ -8,9 +8,14 @@ export const useLocation = () => {
     latitude: 0,
     longitude: 0,
   });
+  const [userLocation, setUserLocation] = useState<Location>({
+    latitude: 0,
+    longitude: 0,
+  });
   useEffect(() => {
     getCurrentLocation().then(location => {
       setInitialPosition(location);
+      setUserLocation(location);
       setHasLocation(true);
     });
   }, []);
@@ -30,9 +35,27 @@ export const useLocation = () => {
       );
     });
   };
+  const followUserLocation = () => {
+    Geolocation.watchPosition(
+      ({ coords }) => {
+        console.log({ coords });
+        setUserLocation({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+      },
+      err => console.log({ err }),
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 10,
+      },
+    );
+  };
   return {
     hasLocation,
     initialPosition,
     getCurrentLocation,
+    followUserLocation,
+    userLocation,
   };
 };
