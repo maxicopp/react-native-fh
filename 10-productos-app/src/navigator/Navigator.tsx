f7734b8ca53ac993a4d6b7742f287c39a39e3273
Maximiliano Cóppola <maxicopp@outlook.com>
2022-03-13 23:42:10 -0300

    Diseño del login

--- 10-productos-app/src/navigator/Navigator.tsx
+++ 10-productos-app/src/navigator/Navigator.tsx
@@ -8,7 +8,11 @@ const Stack = createStackNavigator();
 
 export const Navigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: 'white' },
+      }}>
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
       <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
       <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
