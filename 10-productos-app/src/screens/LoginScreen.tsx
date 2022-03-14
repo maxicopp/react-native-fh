b6b726ba8bcda0722f46d20550aa320273de4e71
Maximiliano Cóppola <maxicopp@outlook.com>
2022-03-14 00:29:35 -0300

    Diseño del login - parte 2

--- 10-productos-app/src/screens/LoginScreen.tsx
+++ 10-productos-app/src/screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, TextInput } from 'react-native';
+import { Platform, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Background } from '../components/Background';
 import { WhiteLogo } from '../components/WhiteLogo';
 import { loginStyles } from '../theme/loginTheme';
@@ -9,15 +10,54 @@ export const LoginScreen = () => {
     <>
       {/* Background */}
       <Background />
-      {/* Keyboard avoid view */}
-      <WhiteLogo />
-      <Text style={loginStyles.title}>Login</Text>
-      <Text style={loginStyles.label}>Email</Text>
-      <TextInput
-        placeholder="Ingrese su email:"
-        placeholderTextColor="rgba(255,255,255,0.4)"
-        keyboardType="email-address"
-      />
+      <View style={loginStyles.formContainer}>
+        {/* Keyboard avoid view */}
+        <WhiteLogo />
+        <Text style={loginStyles.title}>Login</Text>
+        <Text style={loginStyles.label}>Email:</Text>
+        <TextInput
+          placeholder="Ingrese su email:"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          keyboardType="email-address"
+          underlineColorAndroid="white"
+          style={[
+            loginStyles.inputField,
+            Platform.OS === 'ios' && loginStyles.inputFieldIOS,
+          ]}
+          selectionColor="white"
+          // TODO: onchange, value
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={loginStyles.label}>Contraseña:</Text>
+        <TextInput
+          placeholder="******"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          underlineColorAndroid="white"
+          style={[
+            loginStyles.inputField,
+            Platform.OS === 'ios' && loginStyles.inputFieldIOS,
+          ]}
+          selectionColor="white"
+          // TODO: onchange, value
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        {/* Botón login */}
+        <View style={loginStyles.buttonContainer}>
+          <TouchableOpacity activeOpacity={0.8} style={loginStyles.button}>
+            <Text style={loginStyles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+        {/* Crear una nueva cuenta */}
+        <View style={loginStyles.newUserContainer}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => console.log('press')}>
+            <Text style={loginStyles.buttonText}>Nueva cuenta </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 };
