25a203525ffdc9d8d36b3bb8583160f0c21879ef
Maximiliano Cóppola <maxicopp@outlook.com>
2022-04-18 02:27:42 -0300

    Conectar AuthContext con el authReducer

--- 10-productos-app/src/context/AuthContext.tsx
+++ 10-productos-app/src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 import { Usuario } from '../interfaces/appInterfaces';
+import { authReducer, AuthState } from './authReducer';
 
 type AuthContextProps = {
   errorMessage: string;
@@ -12,8 +13,25 @@ type AuthContextProps = {
   removeError: () => void;
 };
 
+const authInitialState: AuthState = {
+  status: 'checking',
+  token: null,
+  user: null,
+  errorMessage: '',
+};
+
 export const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider = ({ children }: any) => {
-  return <AuthContext.Provider value={{  }}>{children}</AuthContext.Provider>;
+  const [state, dispatch] = useReducer(authReducer, authInitialState);
+  const signUp = () => {};
+  const signIn = () => {};
+  const logOut = () => {};
+  const removeError = () => {};
+  return (
+    <AuthContext.Provider
+      value={{ ...state, signUp, signIn, logOut, removeError }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
