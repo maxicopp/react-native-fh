3a000ed6bd4ec073819587406177a1101f6f2af8
Maximiliano Cóppola <maxicopp@outlook.com>
2022-04-19 03:56:21 -0300

    Logout

--- 10-productos-app/src/context/AuthContext.tsx
+++ 10-productos-app/src/context/AuthContext.tsx
@@ -65,7 +65,10 @@ export const AuthProvider = ({ children }: any) => {
     }
   };
   const signUp = () => {};
-  const logOut = () => {};
+  const logOut = async () => {
+    await AsyncStorage.removeItem('token');
+    dispatch({ type: 'logout' });
+  };
   const removeError = () => {
     dispatch({ type: 'removeError' });
   };
