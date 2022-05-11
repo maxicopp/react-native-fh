4d776cd1ba8b3f23bf19dd1f777b2823ab4fb0ac
Maximiliano Cóppola <maxicopp@outlook.com>
2022-05-11 03:04:30 -0300

    Diseño de la pantalla de producto

--- 10-productos-app/src/screens/ProductScreen.tsx
+++ 10-productos-app/src/screens/ProductScreen.tsx
@@ -1,5 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { ProductsStackParams } from '../navigator/ProductsNavigator';
 
@@ -14,10 +22,47 @@ export const ProductScreen = ({ route, navigation }: Props) => {
     });
   }, [name, navigation]);
   return (
-    <View>
-      <Text>
-        {id} {name}
-      </Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.label}>Nombre del producto:</Text>
+        {/* TODO: add value and onChangeText to TextInput */}
+        <TextInput placeholder="Producto" style={styles.textInput} />
+        {/* Picker / Selector */}
+        <Text style={styles.label}>Categoría:</Text>
+        {/* TODO: Por hacer */}
+        <Button title="Guardar" onPress={() => {}} color="#5856D6" />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 10,
+          }}>
+          <Button title="Cámara" onPress={() => {}} color="#5856D6" />
+          <View style={{ width: 10 }} />
+          <Button title="Galería" onPress={() => {}} color="#5856D6" />
+        </View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginHorizontal: 20,
+  },
+  label: {
+    fontSize: 18,
+  },
+  textInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    borderColor: 'rgba(0,0,0,0.2)',
+    height: 45,
+    marginTop: 5,
+    marginBottom: 15,
+  },
+});
