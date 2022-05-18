8604e47e44c7307dabf1b0bec15e84388601fb19
Maximiliano Cóppola <maxicopp@outlook.com>
2022-05-18 03:26:25 -0300

    Android: SplashScreen

--- 03-calculadora/src/screens/CalculadoraScreen.tsx
+++ 03-calculadora/src/screens/CalculadoraScreen.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import { styles } from '../theme/appTheme';
 import { BotonCalc } from '../components/BotonCalc';
 import { useCalculadora } from '../hooks/useCalculadora';
@@ -18,6 +19,9 @@ export const CalculadoraScreen = () => {
     btnSumar,
     calcular,
   } = useCalculadora();
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <View style={styles.calculadoraContainer}>
